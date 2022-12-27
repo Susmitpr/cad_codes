5334a5f8f099186965a1f11bbd67c9e0b53e7427
Susmitpr <113661126+Susmitpr@users.noreply.github.com>
2022-12-27 21:49:15 +0530

    Add files via upload

---
+++ adjacency_list/list_main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "list.h"
+
+
+int main()
+{
+	List *test;
+	int count;
+
+	test = initialise_list();
+
+	assert(vertices_counter() == 5);
+
+	add_connections (test);
+
+	display_list(test);
+
+	return 0;
+}
